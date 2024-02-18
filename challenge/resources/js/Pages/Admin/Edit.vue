f6ea0bf016c9f98df6ba21c008a8c8a1f3a092de
Vinicius Kuwakino <vikuwakino@gmail.com>
2024-02-18 11:39:46 -0300

    Admin User's CRUD done

---
+++ challenge/resources/js/Pages/Admin/Edit.vue
@@ -0,0 +1,97 @@
+<script setup lang="ts">
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import {Head, Link, useForm} from '@inertiajs/vue3';
+import UpdateProfileInformationForm from "@/Pages/Profile/Partials/UpdateProfileInformationForm.vue";
+import {ref} from "vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import InputError from "@/Components/InputError.vue";
+import TextInput from "@/Components/TextInput.vue";
+
+const props = defineProps({
+    selectedAdminUser: Object
+})
+
+const name = ref<HTMLInputElement | null>(null);
+const email = ref<HTMLInputElement | null>(null);
+const selectedAdminUserRef = ref<Object>(props.selectedAdminUser)
+
+const form = useForm({
+    name: selectedAdminUserRef.value.name,
+    email: selectedAdminUserRef.value.email,
+});
+
+</script>
+
+<template>
+    <Head title="EditAdminUser" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Edit Admin User</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                    <section class="max-w-xl">
+                        <header>
+                            <h2 class="text-lg font-medium text-gray-900">Edit Admin User</h2>
+
+                            <p class="mt-1 text-sm text-gray-600">
+                                Edit admin user by filling out the form.
+                            </p>
+                        </header>
+
+                        <form @submit.prevent="form.patch(route('update.admin.user', {id: selectedAdminUserRef.id}))" class="mt-6 space-y-6">
+                            <div>
+                                <InputLabel for="name" value="Name" />
+
+                                <TextInput
+                                    id="name"
+                                    type="text"
+                                    class="mt-1 block w-full"
+                                    v-model="form.name"
+                                    required
+                                    autofocus
+                                    autocomplete="name"
+                                />
+
+                                <InputError class="mt-2" :message="form.errors.name" />
+                            </div>
+
+                            <div>
+                                <InputLabel for="email" value="Email" />
+
+                                <TextInput
+                                    id="email"
+                                    type="email"
+                                    class="mt-1 block w-full"
+                                    v-model="form.email"
+                                    required
+                                    autocomplete="username"
+                                />
+
+                                <InputError class="mt-2" :message="form.errors.email" />
+                            </div>
+
+                            <div class="flex items-center gap-4">
+                                <PrimaryButton :disabled="form.processing">Confirm</PrimaryButton>
+
+                                <Transition
+                                    enter-active-class="transition ease-in-out"
+                                    enter-from-class="opacity-0"
+                                    leave-active-class="transition ease-in-out"
+                                    leave-to-class="opacity-0"
+                                >
+                                    <p v-if="form.recentlySuccessful" class="text-sm text-gray-600">Updated.</p>
+                                </Transition>
+                            </div>
+                        </form>
+
+                    </section>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
--- challenge/resources/js/Pages/Admin/Show.vue
+++ challenge/resources/js/Pages/Admin/Show.vue
@@ -1,17 +1,37 @@
 <script setup lang="ts">
-import { Head, Link } from '@inertiajs/vue3';
+import {Head, Link, useForm} from '@inertiajs/vue3';
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-import PrimaryButton from "@/Components/PrimaryButton.vue";
 import CustomButton from "@/Components/CustomButton.vue";
-
-interface AdminUser {
-    name: String,
-    email: String,
-}
+import DangerButton from "@/Components/DangerButton.vue";
+import SecondaryButton from "@/Components/SecondaryButton.vue";
+import Modal from "@/Components/Modal.vue";
+import {ref} from "vue";
 
 defineProps({
-    users: Array<AdminUser>
+    users: Array,
+    id: Number
 })
+
+const confirmingUserDeletion = ref(false);
+const userDeletion = ref<Object>(null);
+const form = useForm({});
+
+
+const confirmUserDeletion = (user) => {
+    confirmingUserDeletion.value = true;
+    userDeletion.value = user
+};
+
+const closeModal = () => {
+    confirmingUserDeletion.value = false;
+};
+
+function deleteAdminUser() {
+    // console.log("Deleting id: " + userDeletion.value['id'])
+    form.delete(route('destroy.admin.user', userDeletion.value['id']), {
+        onSuccess: () => closeModal()
+    });
+}
 
 </script>
 
@@ -34,14 +54,6 @@ defineProps({
                         </CustomButton>
                     </Link>
 
-<!--                    <Transition-->
-<!--                        enter-active-class="transition ease-in-out"-->
-<!--                        enter-from-class="opacity-0"-->
-<!--                        leave-active-class="transition ease-in-out"-->
-<!--                        leave-to-class="opacity-0"-->
-<!--                    >-->
-<!--                        <p v-if="form.recentlySuccessful" class="text-sm text-gray-600">Saved.</p>-->
-<!--                    </Transition>-->
                 </div>
             </div>
         </template>
@@ -76,12 +88,16 @@ defineProps({
                                             </td>
                                             <td class="px-6 py-4">
                                                 <div class="flex space-x-4 ">
-                                                    <button>
-                                                        <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 512 512">
-                                                            <path fill="#FFD43B" d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/>
-                                                        </svg>
-                                                    </button>
-                                                    <button>
+                                                    <Link :href="route('edit.admin.user', {id: user['id']})">
+                                                        <button>
+                                                            <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 512 512">
+                                                                <path fill="#FFD43B" d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/>
+                                                            </svg>
+                                                        </button>
+                                                    </Link>
+
+<!--                                                    v-on:click="deleteAdminUser(user['id'])"-->
+                                                    <button @click="confirmUserDeletion(user)">
                                                         <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 448 512">
                                                             <path fill="#e84545" d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/>
                                                         </svg>
@@ -90,6 +106,26 @@ defineProps({
                                             </td>
                                         </tr>
                                     </template>
+
+                                    <Modal :show="confirmingUserDeletion" @close="closeModal">
+                                        <div class="p-6">
+                                            <h2 class="text-lg font-medium text-gray-900">
+                                                Are you sure you want to delete {{userDeletion['name']}}?
+                                            </h2>
+
+                                            <div class="mt-6 flex justify-center">
+                                                <SecondaryButton @click="closeModal"> Cancel </SecondaryButton>
+
+                                                <DangerButton
+                                                    class="ms-3"
+                                                    @click="deleteAdminUser"
+                                                >
+                                                    Delete Admin User
+                                                </DangerButton>
+                                            </div>
+                                        </div>
+                                    </Modal>
+
                                 </tbody>
                             </table>
                         </div>
