79ab7d59afa6ad05ea1e6a2adbf661ca9e256e4a
Vinicius Kuwakino <vikuwakino@gmail.com>
2024-02-18 11:44:56 -0300

    Implementing Admin User Service

--- challenge/app/Services/AdminsService.php
+++ challenge/app/Services/AdminsService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Models\User;
 use App\Repositories\IAdminsRepository;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Auth;
@@ -16,4 +17,27 @@ class AdminsService
     {
         return $this->adminsRepository->listAll()->except(Auth::id());
     }
+
+    public function findAdminById(string $id): array
+    {
+        return $this->adminsRepository->findById($id)->toArray();
+    }
+
+    public function createAdmin(Array $req): void
+    {
+        $this->adminsRepository->create($req);
+    }
+
+    public function updateAdmin(string $id, Array $req): void
+    {
+        $adminUser = $this->adminsRepository->findById($id);
+        $this->adminsRepository->update($adminUser, $req);
+    }
+
+    public function deleteAdmin(string $id): void
+    {
+        $adminUser = $this->adminsRepository->findById($id);
+        $this->adminsRepository->delete($adminUser);
+    }
+
 }
