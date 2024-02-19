f82277dd25a2085d8052f472d385360d096611d8
Vinicius Kuwakino <vikuwakino@gmail.com>
2024-02-19 01:32:51 -0300

    Add Github Users Repository binding

--- challenge/app/Providers/RepositoriesProvider.php
+++ challenge/app/Providers/RepositoriesProvider.php
@@ -3,12 +3,15 @@
 namespace App\Providers;
 
 use App\Repositories\EloquentAdminsRepository;
+use App\Repositories\EloquentGithubUsersRepository;
 use App\Repositories\IAdminsRepository;
+use App\Repositories\IGithubUsersRepository;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoriesProvider extends ServiceProvider
 {
     public array $bindings = [
-        IAdminsRepository::class => EloquentAdminsRepository::class
+        IAdminsRepository::class => EloquentAdminsRepository::class,
+        IGithubUsersRepository::class => EloquentGithubUsersRepository::class
     ];
 }
