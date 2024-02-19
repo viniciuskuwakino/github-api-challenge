75224c7168cdb1d8fd72de179144445734797f74
Vinicius Kuwakino <vikuwakino@gmail.com>
2024-02-19 01:29:38 -0300

    Add routes of Github Users

--- challenge/routes/web.php
+++ challenge/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AdminUserController;
+use App\Http\Controllers\GithubUserController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -32,12 +33,19 @@ Route::get('/dashboard', function () {
 
 Route::middleware('auth')->group(function () {
 
-    Route::get('/admin', [AdminUserController::class, 'index'])->name('admin.show');
+    Route::get('/admin', [AdminUserController::class, 'index'])->name('admin.index');
     Route::get('/admin/create', [AdminUserController::class, 'create'])->name('create.admin.user');
     Route::post('/admin/create', [AdminUserController::class, 'store'])->name('store.admin.user');
     Route::get('/admin/{id}', [AdminUserController::class, 'edit'])->name('edit.admin.user');
     Route::patch('/admin', [AdminUserController::class, 'update'])->name('update.admin.user');
     Route::delete('/admin/{id}', [AdminUserController::class, 'destroy'])->name('destroy.admin.user');
+
+    Route::get('/github', [GithubUserController::class, 'index'])->name('github.index');
+    Route::get('/github/{id}', [GithubUserController::class, 'show'])->name('show.github.user');
+    Route::post('/github', [GithubUserController::class, 'store'])->name('store.github.user');
+    Route::get('/github/edit/{id}', [GithubUserController::class, 'edit'])->name('edit.github.user');
+    Route::patch('/github/edit', [GithubUserController::class, 'update'])->name('update.github.user');
+    Route::delete('/github/{id}', [GithubUserController::class, 'destroy'])->name('destroy.github.user');
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
